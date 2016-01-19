31b371ab3484af2aab1030a783212dd8d8734625
Phanx <phanx@phanx.net>
2016-01-19 10:26:30 -0800

    Fetch next episode separately
    This adds a second HTTP request for each show, but still results in a large speedup when shows have many episodes (eg. Doctor Who, SVU) and avoids the possibility that TVmaze will time out when trying to fetch data with embedded episodes for these shows.

--- src/js/app.js
+++ src/js/app.js
@@ -22,46 +22,72 @@ $.getJSON("/api/shows", function(SHOWS) {
 	})
 
 	SHOWS.forEach(function(show) {
-		var URL = "http://api.tvmaze.com/singlesearch/shows?q=" + encodeURIComponent(show.name.toLowerCase()) + "&embed=episodes"
+		var URL = "http://api.tvmaze.com/singlesearch/shows?q=" + encodeURIComponent(show.name.toLowerCase()) // + "&embed=episodes"
 		console.log("Fetching data for show: " + show.name + " @ " + URL)
 
-		$.getJSON(URL, function(show) {
-			numFetched++
+		$.getJSON(URL, function(showdata) {
+			// console.log("----------")
 			console.log("Received data for show: " + show.name + " (" + numFetched + "/" + numShows + ")")
-			console.log(show)
+			// console.log(showdata)
 
-			var i = DAYS.indexOf(show.schedule.days[0] || "Unknown")
-			if (i === -1) {
-				i = DAYS.indexOf("Unknown")
-			}
-			data[i].shows.push(show)
-			data[i].shows.sort(function(a, b) {
-				return a.name.toLowerCase().replace(/^the /, "") > b.name.toLowerCase().replace(/^the /, "")
-			})
-
-			if (numFetched === numShows) {
-				console.log("All data received!")
-				console.log(data)
-
-				DAYS.pop() // remove "Unknown"
-				DAYS.unshift(DAYS.pop()) // move "Sunday" from end to start
-				var date = new Date()
-				var day = date.getDay()
-				if (date.getHours() < 17) {
-					// Before 9 PM, show yesterday instead of today
-					day = (day > 0) ? (day - 1) : (DAYS.length - 1)
+			if (showdata._links.nextepisode) {
+				// console.log("Fetching next episode @ " + showdata._links.nextepisode.href)
+				$.getJSON(showdata._links.nextepisode.href, function(episode) {
+					// console.log("Received next episode for show: " + show.name)
+					numFetched++
+					showdata._embedded = {
+						episodes: [ episode ]
+					}
+					addDataForShow(showdata)
+				})
+			} else {
+				// console.log("No next episode")
+				numFetched++
+				showdata._embedded = {
+					episodes: []
 				}
-
-				ReactDOM.render(
-					<ShowList data={data} initialDay={DAYS[day]} />,
-					document.getElementById("content")
-				)
+				addDataForShow(showdata)
 			}
 		})
 	})
+
+	function addDataForShow(showdata) {
+		var i = DAYS.indexOf(showdata.schedule.days[0] || "Unknown")
+		if (i === -1) {
+			i = DAYS.indexOf("Unknown")
+		}
+		data[i].shows.push(showdata)
+		data[i].shows.sort(function(a, b) {
+			return a.name.toLowerCase().replace(/^the /, "") > b.name.toLowerCase().replace(/^the /, "")
+		})
+
+		if (numFetched === numShows) {
+			console.log("----------")
+			console.log("All data received!")
+			//console.log(data)
+			processShowData()
+		}
+	}
+
+	function processShowData() {
+		DAYS.pop() // remove "Unknown"
+		DAYS.unshift(DAYS.pop()) // move "Sunday" from end to start
+		var date = new Date()
+		var day = date.getDay()
+		if (date.getHours() < 17) {
+			// Before 9 PM, show yesterday instead of today
+			day = (day > 0) ? (day - 1) : (DAYS.length - 1)
+		}
+
+		ReactDOM.render(
+			<ShowList data={data} initialDay={DAYS[day]} />,
+			document.getElementById("content")
+		)
+	}
 })
 
 ReactDOM.render(
 	<LoadingDots />,
 	document.getElementById("content")
 )
+
